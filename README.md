4bcfdeac3735a9588978f60cb9016c35e21f97f2
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-02-18 17:28:45 +0600

    feat: 🎸 starting... 🪝useCallback
    starting... 🪝useCallback

---
+++ README.md
@@ -0,0 +1,23 @@
+# REACT HOOKS 🪝 + ![vit svg](/public/vite.svg)
+
+![React hooks thumbnail for README.md](public/images/reacthooks.png)
+
+---
+
+## Here is the list of available React hooks from their documentation 📃
+
+        1. useSTate most frequently use
+        2. useCallback
+        3. useContext
+        4. useDebugValue
+        5. useDeferredValue
+        6. useEffect
+        7. useId
+        8. useImperativeHandle
+        9. useInsertionEffect
+        10. useLayoutEffect
+        11. useMemo
+        12. useReducer
+        13. useRef
+        14. useSyncExternalStore
+        15. useTransition
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userInput, setUserInput] = useState("");
+  const [num] = useState(5);
+  const [num1] = useState(4);
 
+  const sum = () => num + num1;
+  useEffect(() => {
+    console.log("mounted");
+    console.log(sum());
+    return () => console.log("unmounted");
+  }, [sum]);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>React Hooks</h1>
+      <input
+        style={{ padding: ".45rem 1.5rem" }}
+        type="text"
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+
+      <p style={{ fontSize: "2.2rem" }}>user are typing = {userInput}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
